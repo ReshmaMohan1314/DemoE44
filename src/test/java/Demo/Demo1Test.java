ef65c65f1ba09a33e069759aaa77996e66ffc19d
venka <venka@VenkatMS_07>
2026-07-10 10:07:49 -0400

    Committed

---
+++ src/test/java/Demo/Demo1Test.java
@@ -0,0 +1,17 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test 
+{
+	
+	@Test(groups = "regression")
+	public void movie1Test()
+	{
+		
+		System.out.println("Ghilli");
+	}
+	
+	
+
+}
---
+++ src/test/java/Demo/DemoTest.java
@@ -0,0 +1,17 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest
+{
+	
+	@Test(groups = "smoke")
+	public void movieTest()
+	{
+		System.out.println("Leo");
+			
+	}
+	
+	
+	
+}
